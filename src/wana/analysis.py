8059861f7a7f38603b042cc1f22a4f9ae90d5859
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-09-15 19:58:22 +0200

    detect steps

--- src/wana/analysis.py
+++ src/wana/analysis.py
@@ -27,7 +27,70 @@ def flag_resting(sensor):
     kernel = np.ones(N_kernel)/N_kernel
     conv = np.convolve(mask, kernel, mode="same")
     sensor.data["mask_resting"] = conv > 0.5
+    sensor.data["mask_moving"] = conv <= 0.5
+    
+    find_resting_edge(sensor)
         
+def find_resting_edge(sensor):
+    """ Detect the beginning of a resting interval.
+
+    Adds 4 arrays:
+        mask_start/stop_resting: boolean array with 1 when resting starts/stops
+        index_start/stop_resting: integer array with indices when resting starts/stops
+
+    Parameters
+    ----------
+    sensor: wana.sensor.Sensor
+        Sensor object holding the data.
+    """
+    resting = sensor.data["mask_resting"]
+    
+    trigger_val = 0.5
+    mask1 = (resting[:-1] < trigger_val) & (resting[1:] > trigger_val)
+    mask2 = (resting[:-1] > trigger_val) & (resting[1:] < trigger_val)
+
+    index_start_resting = np.flatnonzero(mask1)+1
+    mask_start_resting = np.zeros(len(resting))
+    mask_start_resting[index_start_resting] = 1
+    
+    sensor.data["index_start_resting"] = index_start_resting
+    sensor.data["mask_start_resting"] = mask_start_resting
+
+    index_stop_resting = np.flatnonzero(mask2)+1
+    mask_stop_resting = np.zeros(len(resting))
+    mask_stop_resting[index_stop_resting] = 1
+        
+    sensor.data["index_stop_resting"] = index_stop_resting
+    sensor.data["mask_stop_resting"] = mask_stop_resting
+
+def find_step_intervals(sensor):
+    """ Detect the bounding interval of steps.
+
+    Adds 4 arrays:
+        mask_start/stop_resting: boolean array with 1 when resting starts/stops
+        index_start/stop_resting: integer array with indices when resting starts/stops
+
+    Parameters
+    ----------
+    sensor: wana.sensor.Sensor
+        Sensor object holding the data.
+    """
+    steps = []
+    index_stop = sensor.data["index_start_resting"]
+    index_start = sensor.data["index_stop_resting"]
+
+    if index_start[0] > index_stop[0]:
+        index_stop = index_stop[1:]
+    
+    N_start = len(index_start)
+    N_stop = len(index_stop)
+    
+    N_steps = min(N_start, N_stop)
+    
+    for n in range(N_steps):
+        steps.append( [index_start[n], index_stop[n]])
+    
+    sensor.data["interval_steps"] = np.array(steps)
 
 
 def estimate_g(sensor):
@@ -42,7 +105,6 @@ def estimate_g(sensor):
     sensor: wana.sensor.Sensor
         Sensor object holding the data.
     """
-    flag_resting(sensor)
     mask_moving = np.logical_not(sensor.data["mask_resting"])
     for d in ["x", "y", "z"]:
         a_masked = ma.masked_array(sensor.data["iss_a" + d], mask=mask_moving)
