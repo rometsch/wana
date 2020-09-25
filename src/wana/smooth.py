5c8a5be17c6faf09c5c0b53650ac5c5c96ebcaf2
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-09-25 16:50:40 +0200

    add lowpass filter

--- src/wana/smooth.py
+++ src/wana/smooth.py
@@ -1,9 +1,11 @@
 """ Function to smooth the data. """
 import numpy as np
+from scipy import signal
+
 
 def smooth_window(sensor, varname, N=10):
     """ Run a moving average over the data.
-    
+
     Parameres
     ---------
     sensor: wana.sensor.Sensor
@@ -14,8 +16,27 @@ def smooth_window(sensor, varname, N=10):
         Length of the smoothing interval.
     """
     v = sensor.data[varname]
-    
+
+    N = int(N)
     kernel = np.ones(N)/N
-    
-    sensor.data[varname] = np.convolve(v,kernel, mode="same")
-    
+
+    sensor.data[varname] = np.convolve(v, kernel, mode="same")
+
+
+def smooth_lowpass(sensor, varname, fcrit=10):
+    """ Run a lowpass filter over the data.
+
+    Parameres
+    ---------
+    sensor: wana.sensor.Sensor
+        Sensor object holding the data.
+    varname: str
+        Variable to smooth.
+    """
+    dt = sensor.data["dt"][0]
+    sig = sensor.data[varname]
+    sampling_rate = 1/dt
+    filter_order = 10
+    sos = signal.butter(filter_order, fcrit, 'lp', fs=sampling_rate, output='sos')
+    filtered = signal.sosfilt(sos, sig)
+    sensor.data[varname] = filtered
